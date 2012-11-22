8dd855f97f349e7692bccdad5d306993e810fd97
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2012-11-22 09:59:40 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uccloud::Application.config.secret_token = '7b228c4ab509bc8a193aa1cb2bd66fa31a6861abcb3e7ff66ab9fd5fc9b59240711e1cd7e7c57f85011fc4d3395548a30e4c35d3292926fa7a825769be922dd7'
