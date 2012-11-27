377b2979ff1da3ff5c6c0602ebfb25ef310dcf63
Hector Enrique Gomez Morales <hectoregm@gmail.com>
2012-11-27 11:27:22 -0600

    Disable migrate for now.

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ before 'deploy:setup', 'rvm:install_rvm'
 before 'deploy:setup', 'rvm:install_ruby'
 
 # Callbacks after deploy:restart
-after "bundle:install", "deploy:migrate"
+#after "bundle:install", "deploy:migrate"
 after "deploy:restart", "deploy:cleanup"
 
 namespace :deploy do
